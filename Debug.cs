fdd8ded600586c7cc58de3271acd8b567a209a00
komp\Admin <skorakora@wp.pl>
2023-01-30 02:56:31 +0000

    Added debuger v 1.0

---
+++ Debug.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zielona_Alkaida_Launcher.Core;
+
+namespace Updater.Core
+{
+    public static class Debug
+    {
+        public static bool debugConsole = true;
+
+        static FileStream file;
+        public static void init(string logPath)
+        {
+            if(File.Exists(logPath))
+            {
+                File.Delete(logPath);
+            }
+
+            file = new FileStream(logPath, FileMode.Create);
+
+            if (true)
+            {
+                WinConsole.Initialize();
+            }
+
+
+        }
+
+        public static void Log(string text)
+        {
+            byte[] data = StringToByte(text);
+
+            file.Write(data, 0, data.Length);
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("[" + DateTime.Now.ToString("h:mm:ss tt") + "]" + " " + text);
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        public static void Warning(string text)
+        {
+            byte[] data = StringToByte(text);
+
+            file.Write(data, 0, data.Length);
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("[" + DateTime.Now.ToString("h:mm:ss tt") + "]" + " [WARNING!] " + text);
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        public static void Error(string text)
+        {
+            byte[] data = StringToByte(text);
+
+            file.Write(data, 0, data.Length);
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("["+DateTime.Now.ToString("h:mm:ss tt")+"]"+" [ERROR!] "+text);
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        public static void Write(string text)
+        {
+            byte[] data = StringToByte(text);
+
+            file.Write(data, 0, data.Length);
+
+            Console.WriteLine(text);
+        }
+
+        public static void ShowConsole()
+        {
+            if (debugConsole)
+            {
+                return;
+            }
+
+            WinConsole.Initialize();
+
+        }
+
+        public static void HideConsole()
+        {
+            if (debugConsole)
+            {
+                return;
+            }
+
+            //hideconsole code
+        }
+
+        private static byte[] StringToByte(string text)
+        {
+            return Encoding.UTF8.GetBytes(text);
+        }
+
+        private static byte[] ASCIIStringToByte(string text)
+        {
+            return Encoding.UTF8.GetBytes(text);
+        }
+    }
+}
